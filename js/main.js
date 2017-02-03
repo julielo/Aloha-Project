f04e68cac561efa667ab25783c75d79c75f78f5b
julielo5 <julielo.inbox@gmail.com>
2017-02-02 17:39:22 -0800

    Added .ready() function around my JavaScript code.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,41 @@
+$(function () {
 
-/* Smooth Scrolling */
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
+  /* Smooth Scrolling */
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
       return false;
+      }
     }
+  });
+
+  /* Flickity plugin */
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+
+  /* Email validation for Update Section */
+  function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
   }
-});
 
-/* Flickity plugin */
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-/* Email validation for Update Section */
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
-
-$('form').on('click', '.subscribe', function(){
+  $('form').on('click', '.subscribe', function(){
    console.log(this)
    event.preventDefault();
-var textBoxVal = $('.userInput').val();
+   var textBoxVal = $('.userInput').val();
 
-if(isEmail(textBoxVal)){
-  alert('Thanks for Subscribing!');
-}else{
-  alert('Please enter a valid email address!');
+   if(isEmail(textBoxVal)){
+    alert('Thanks for Subscribing!');
+   }else{
+    alert('Please enter a valid email address!');
+   }
+  });
 }
-});
